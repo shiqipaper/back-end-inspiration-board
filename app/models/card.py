62e035e52ebbde27051d1cbaa51c10598f6ef0e3
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-12-23 06:33:15 -0600

    updated likes_count

--- app/models/card.py
+++ app/models/card.py
@@ -1,11 +1,12 @@
 from ..db import db
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
+from typing import Optional
 
 class Card(db.Model):
     card_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     message: Mapped[str]
-    likes_count: Mapped[int]
+    likes_count: Mapped[Optional[int]]
     board_id: Mapped[int] = mapped_column(ForeignKey("board.board_id"))
     board: Mapped["Board"] = relationship(back_populates="cards")
 
@@ -13,7 +14,7 @@ class Card(db.Model):
         card_as_dict = dict(
             card_id=self.card_id,
             message=self.message,
-            likes_count=self.likes_count,
+            likes_count=self.likes_count if self.likes_count else 0,
             board_id=self.board_id,
         )
         return card_as_dict
@@ -22,6 +23,6 @@ class Card(db.Model):
     def from_dict(cls, card_data):
         return cls(
             message=card_data["message"],
-            likes_count=card_data["likes_count"],
+            likes_count=card_data.get("likes_count", 0),
             board_id=card_data["board_id"],
         )
